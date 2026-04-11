7ad40327c7f3e2fc6c0652050290df5c2c0c7212
Elior <heliosaian@gmail.com>
2026-04-11 18:32:48 +0300

    Add 'FadeTransition.vue' component for UI animations

---
+++ src/components/common/FadeTransition.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+
+<template>
+  <Transition appear>
+    <slot></slot>
+  </Transition>
+</template>
+
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.3s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+  transform: translateY(-20px);
+}
+</style>
