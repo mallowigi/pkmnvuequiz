4139e75f5aad785e597ef2de6c174f69c929d3e6
Elior <heliosaian@gmail.com>
2026-06-03 21:23:56 +0300

    Add `vue-i18n` for internationalization in `main.ts`

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,37 @@ import './assets/main.css';
 import { MotionPlugin } from '@vueuse/motion';
 import { createPinia } from 'pinia';
 import { createApp } from 'vue';
+import { createI18n } from 'vue-i18n';
 
 import gameEnded from '@/directives/gameEnded.ts';
 import tooltip from '@/directives/tooltip.ts';
 import { piniaStorePlugin } from '@/stores/piniaStorePlugin.ts';
+import { capitalize } from '@/utils/utils.ts';
 
 import App from './App.vue';
+
+const i18n = createI18n({
+  fallbackLocale: 'en',
+  locale: 'fr',
+  messages: {
+    en: {
+      nameAllRegionPokemon: 'Name all {regionOrType} Pokémon:',
+    },
+    fr: {
+      nameAllRegionPokemon: 'Nommez tous les Pokémon de {regionOrType}:',
+    },
+  },
+  modifiers: {
+    capitalize: (value) => capitalize(value.toString()),
+  },
+});
 
 const app = createApp(App);
 const pinia = createPinia();
 
 app.use(pinia);
 app.use(MotionPlugin);
+app.use(i18n);
 app.directive('tooltip', tooltip);
 app.directive('game-ended', gameEnded);
 
