efa579f906583cc81ad1b379486ddb7c307a5ad5
Elior <heliosaian@gmail.com>
2026-06-03 23:21:35 +0300

    Move i18n messages to separate `messages.json` file for better maintainability, and update `main.ts` to use the externalized messages.

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { createI18n } from 'vue-i18n';
 
 import gameEnded from '@/directives/gameEnded.ts';
 import tooltip from '@/directives/tooltip.ts';
+import messages from '@/locales/messages.json';
 import { piniaStorePlugin } from '@/stores/piniaStorePlugin.ts';
 import { capitalize } from '@/utils/utils.ts';
 
@@ -14,81 +15,7 @@ import App from './App.vue';
 const i18n = createI18n({
   fallbackLocale: 'en',
   locale: 'en',
-  messages: {
-    cn: {
-      alola: '阿罗拉',
-      galar: '伽勒尔',
-      hoenn: '丰缘',
-      johto: '城都',
-      kalos: '卡洛斯',
-      kanto: '关都',
-      paldea: '帕尔迪亚',
-      sinnoh: '神奥',
-      unova: '合众',
-    },
-    de: {},
-    en: {
-      alola: 'Alola',
-      galar: 'Galar',
-      hoenn: 'Hoenn',
-      johto: 'Johto',
-      kalos: 'Kalos',
-      kanto: 'Kanto',
-      paldea: 'Paldea',
-      sinnoh: 'Sinnoh',
-      unova: 'Unova',
-    },
-    es: {},
-    fr: {
-      unova: 'Unys',
-    },
-    it: {},
-    jp: {
-      alola: 'アローラ',
-      galar: 'ガラル',
-      hoenn: 'ホウエン',
-      johto: 'ジョウト',
-      kalos: 'カロス',
-      kanto: 'カントー',
-      paldea: 'パルデア',
-      sinnoh: 'シンオウ',
-      unova: 'イッシュ',
-    },
-    ko: {
-      alola: '알로라',
-      galar: '가라르',
-      hoenn: '호연',
-      johto: '조토',
-      kalos: '칼로스',
-      kanto: '관동',
-      paldea: '팔데아',
-      sinnoh: '신오',
-      unova: '이상',
-    },
-    pt: {},
-    ru: {
-      alola: 'Ало́ла',
-      galar: 'Галар',
-      hoenn: 'Хоэн',
-      johto: 'Джото',
-      kalos: 'Калос',
-      kanto: 'Канто',
-      paldea: 'Палдея',
-      sinnoh: 'Синно',
-      unova: 'Юнова',
-    },
-    zh: {
-      alola: '阿罗拉',
-      galar: '伽勒尔',
-      hoenn: '丰缘',
-      johto: '城都',
-      kalos: '卡洛斯',
-      kanto: '关都',
-      paldea: '帕尔迪亚',
-      sinnoh: '神奥',
-      unova: '合众',
-    },
-  },
+  messages,
   modifiers: {
     capitalize: (value) => capitalize(value.toString()),
   },
