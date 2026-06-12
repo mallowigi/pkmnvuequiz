650c768e6e6e031b24343b899c7f7d0c24ffadb2
Elior <heliosaian@gmail.com>
2026-06-12 22:51:45 +0300

    Add 'useLeaderboards' composable for centralized logic

---
+++ src/composables/useLeaderboards.ts
@@ -0,0 +1,39 @@
+import { useFirebase } from '@/composables/useFirebase.ts';
+import { useCurrentGen } from '@/stores/useCurrentGen.ts';
+import { useCurrentType } from '@/stores/useCurrentType.ts';
+import { useState } from '@/stores/useState.ts';
+import type { GameMode, Gen, Type } from '@/types.ts';
+
+type LeaderboardsProps = {
+  uid?: string | null;
+  gameMode?: GameMode | null;
+  gen?: Gen | null;
+  type?: Type | null;
+};
+
+export const useLeaderboards = () => {
+  const { currentGenState } = useCurrentGen();
+  const { currentTypeState } = useCurrentType();
+  const { state } = useState();
+  const { getTopTrainers } = useFirebase();
+
+  const getLeaderboards = (props: LeaderboardsProps) => {
+    const gameMode = props.gameMode ?? state.gameMode!;
+    const gen = props.gen ?? currentGenState.gen;
+    const type = props.type ?? currentTypeState.currentType;
+    const uid = props.uid;
+
+    const topTrainers = getTopTrainers({
+      gameMode,
+      gen,
+      type,
+      uid,
+    });
+
+    return topTrainers;
+  };
+
+  return {
+    getLeaderboards,
+  };
+};
